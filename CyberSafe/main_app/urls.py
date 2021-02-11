77b03d8584d35faa837e10d2c612a5b32b12d50b
Spencer Saunders <spencer.saunders45@gmail.com>
2021-02-11 17:34:15 -0600

    Added create secret, delete secret, and update secret

--- CyberSafe/main_app/urls.py
+++ CyberSafe/main_app/urls.py
@@ -5,10 +5,10 @@ urlpatterns = [
     path('login', views.login),
     path('register', views.createUser),
     path('', views.main),
-    path('update/', views.updateSecret), #add id to url
-    path('delete/', views.deleteSecret), #add id to url
+    path('update/<int:secretId>', views.updateSecret), #add id to url
+    path('delete/<int:secretId>', views.deleteSecret), #add id to url
     path('create', views.createSecret),
-    path('view/', views.viewSecret), #add id to url
+    path('view/<int:secretId>', views.viewSecret), #add id to url
     path('update', views.updateUser),
     path('delete', views.deleteUser)
 ]
--- CyberSafe/main_app/views.py
+++ CyberSafe/main_app/views.py
@@ -65,7 +65,10 @@ def addUser(request):
 # loads the page to display and edit user info
 def updateUser(request):
     logged_in_user = Users.objects.get(id=request.session['user_id'])
-    return render(request, 'update_user.html')
+    context = {
+        'logged_in_user': logged_in_user
+    }
+    return render(request, 'update_user.html', context)
 
 # updates the user info
 def updateUserInfo(request):
@@ -97,26 +100,77 @@ def deleteUser(request):
 
 
 # -----> Secret Related Data <-----
+
+# loads the main page
 def main(request):
     logged_in_user = Users.objects.get(id=request.session['user_id'])
-    return render(request, 'main.html')
+    context = {
+        'logged_in_user': logged_in_user
+    }
+    return render(request, 'main.html', context)
 
-
-def updateSecret(request):
+# loads the page to update a secret
+def updateSecret(request, secret_id):
     logged_in_user = Users.objects.get(id=request.session['user_id'])
-    return render(request, 'update_secret.html')
+    secret = Secrets.objects.get(id=secret_id)
+    context = {
+        'secret': secret
+    }
+    return render(request, 'update_secret.html', context)
 
-
-def deleteSecret(request):
+# updates the secret
+def updateSecretData(request, secret_id):
     logged_in_user = Users.objects.get(id=request.session['user_id'])
-    return render(request)
+    # encrypts username before saving it to the DB
+    username = request.POST['username']
+    hashed_un = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+    # encrypts password before saving it to the DB
+    password = request.POST['password']
+    hashed_pw = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+    # saves the data to the DB
+    form = request.POST
+    this_secret = Users.objects.get(id=secret_id)
+    this_secret.site = form['site']
+    this_secret.username = hashed_un
+    this_secret.password = hashed_pw
+    this_secret.save()
+    return redirect('/')
 
+# deletes secret from DB
+def deleteSecret(request, secret_id):
+    logged_in_user = Users.objects.get(id=request.session['user_id'])
+    this_secret = Secrets.objects.get(id=secret_id)
+    this_secret.delete()
+    return redirect('/')
 
+# loads the page to create a secret
 def createSecret(request):
     logged_in_user = Users.objects.get(id=request.session['user_id'])
     return render(request, 'create_secret.html')
 
-
-def viewSecret(request):
+# Takes the POST data and creates a secret
+def createSecretData(request):
     logged_in_user = Users.objects.get(id=request.session['user_id'])
-    return render(request, 'view_secret.html')
+    # encrypts the password before saving it in the DB
+    username = request.POST['username']
+    hashed_un = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+    # encrypts the password before saving it in the DB
+    password = request.POST['password']
+    hashed_pw = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+    # adds the secret into the DB
+    form = request.POST
+    new_user = Users.objects.create(
+        site = form['site'],
+        username = form['username'],
+        password = form['password'],
+    )
+    return redirect('/')
+
+# loads the page to view secret info
+def viewSecret(request, secret_id):
+    logged_in_user = Users.objects.get(id=request.session['user_id'])
+    secret = Secrets.objects.get(id=secret_id)
+    context = {
+        'secret': secret
+    }
+    return render(request, 'view_secret.html', context)
